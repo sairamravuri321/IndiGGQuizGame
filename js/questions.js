c3a0da1658a6fdce5234337d699f9a9b20c23e5b
sairam <satyasairavuri@gmail.com>
2023-10-02 11:18:12 +0530

    quiz

---
+++ js/questions.js
@@ -0,0 +1,61 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "which of the following is a programming languague?",
+    answer: "Pascal",
+    options: [
+      "Lotus",
+      "Pascal",
+      "Ms-Excel",
+      "Netscape"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Cobol,Pascal,Fortran are all ",
+    answer: "Programming Languages",
+    options: [
+      "Tribes of America",
+      "Languages of various nomadic tribes",
+      "Programming Languages",
+      "None of these"
+    ]
+  },
+    {
+      numb: 3,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+    
+  },
+    {
+    
+    numb: 4,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+];
